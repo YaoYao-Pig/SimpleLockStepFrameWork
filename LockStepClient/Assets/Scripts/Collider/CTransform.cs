36d72e30d9f8dba8f6f7e97d7ce986f5c7796a32
摇摇猪 <73538420+YaoYao-Pig@users.noreply.github.com>
2025-05-16 22:02:55 +0800

    feat: add basic collider

---
+++ LockStepClient/Assets/Scripts/Collider/CTransform.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Lockstep.Math;
+using UnityEngine;
+
+public class CTransform
+{
+    public LVector2 position;
+
+    public CTransform(LVector2 position)
+    {
+        this.position = position;
+    }
+    
+}
---
+++ LockStepClient/Assets/Scripts/Collider/ColliderProxy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Lockstep.Math;
+using UnityEngine;
+
+public class ColliderProxy
+{
+
+    public CTransform transform;
+    
+}
